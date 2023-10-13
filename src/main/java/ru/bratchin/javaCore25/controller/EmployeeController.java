482437e8f98aed1f34029ffcf2e1c5a4314632c9
aleks <alexbratchin@gmail.com>
2023-10-13 19:28:20 +0300

    Сделал ДЗ

--- src/main/java/ru/bratchin/javaCore25/controller/EmployeeController.java
+++ src/main/java/ru/bratchin/javaCore25/controller/EmployeeController.java
@@ -1,16 +1,18 @@
 package ru.bratchin.javaCore25.controller;
 
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.bratchin.javaCore25.model.entity.Employee;
 import ru.bratchin.javaCore25.service.api.EmployeeService;
 import ru.bratchin.javaCore25.service.impl.EmployeeMaxSizeTenService;
 
 @RestController
 @RequestMapping("/employee")
+@Validated
 public class EmployeeController {
 
     private final EmployeeService service;
@@ -21,8 +23,8 @@ public class EmployeeController {
 
     @GetMapping("/add")
     public ResponseEntity<?> add(
-            @RequestParam String name,
-            @RequestParam String surname
+            @RequestParam @NotBlank String name,
+            @RequestParam @NotBlank String surname
     ) {
         Employee employee = new Employee(surname, name);
         return ResponseEntity.ok(service.add(employee));
@@ -30,8 +32,8 @@ public class EmployeeController {
 
     @GetMapping("/delete")
     public ResponseEntity<?> delete(
-            @RequestParam String name,
-            @RequestParam String surname
+            @RequestParam @NotBlank String name,
+            @RequestParam @NotBlank String surname
     ) {
         Employee employee = new Employee(surname, name);
         return ResponseEntity.ok(service.delete(employee));
@@ -39,11 +41,17 @@ public class EmployeeController {
 
     @GetMapping("/find")
     public ResponseEntity<?> find(
-            @RequestParam String name,
-            @RequestParam String surname
+            @RequestParam @NotBlank String name,
+            @RequestParam @NotBlank String surname
     ) {
         Employee employee = new Employee(surname, name);
         return ResponseEntity.ok(service.find(employee));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void errorParam() {
+        System.out.println("sd");
     }
 
 }
--- src/test/java/ru/bratchin/javaCore25/controller/EmployeeControllerTest.java
+++ src/test/java/ru/bratchin/javaCore25/controller/EmployeeControllerTest.java
@@ -33,89 +33,18 @@ class EmployeeControllerTest {
     private TestRestTemplate restTemplate;
 
     private static Field fieldEmployees;
-    @BeforeAll
-    public static void setup() throws NoSuchFieldException {
-        fieldEmployees = EmployeeMaxSizeTenService.class.getDeclaredField("employees");
-        fieldEmployees.setAccessible(true);
-    }
 
-    @BeforeEach
-    public void initEach() throws IllegalAccessException {
-        var testEmployees = new ArrayList<>(
-                List.of(
-                        new Employee("Малышева", "Амалия"),
-                        new Employee("Козловский", "Денис"),
-                        new Employee("Соловьева", "Серафима"),
-                        new Employee("Макарова", "Дарья"),
-                        new Employee("Лебедева", "Таисия"),
-                        new Employee("Романов", "Артём"),
-                        new Employee("Широков", "Павел"),
-                        new Employee("Кудрявцев", "Лев"),
-                        new Employee("Филиппова", "Алиса")
-                )
-        );
-        fieldEmployees.set(service, testEmployees);
-    }
-    @Nested
-    class AllSuccess{
-        @Test
-        void add() throws IllegalAccessException {
-            Employee testEmployee = new Employee("Белякова", "Антонина");
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
-
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
-            var employees = (List<Employee>) fieldEmployees.get(service);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
-            assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
-            assertThat(employees.size()).isEqualTo(10);
-            assertThat(employees).contains(testEmployee);
-        }
-        @Test
-        void delete() throws IllegalAccessException {
-            Employee testEmployee = new Employee("Кудрявцев", "Лев");
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
-
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/delete?name={name}&surname={surname}", Employee.class, uriVariables);
-            var employees = (List<Employee>) fieldEmployees.get(service);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
-            assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
-            assertThat(employees.size()).isEqualTo(8);
-            assertThat(employees).doesNotContain(testEmployee);
-        }
-        @Test
-        void find() throws IllegalAccessException {
-            Employee testEmployee = new Employee("Кудрявцев", "Лев");
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
-
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/find?name={name}&surname={surname}", Employee.class, uriVariables);
-            var employees = (List<Employee>) fieldEmployees.get(service);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
-            assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
-            assertThat(employees).contains(testEmployee);
-            assertThat(employees.size()).isEqualTo(9);
-        }
-    }
 
     @Nested
-    class AllError{
-        @Test
-        void addStorageIsFull() throws IllegalAccessException{
-            Employee testEmployee = new Employee("Иванов", "Иван");
+    class TestAddDeleteFind {
+        @BeforeAll
+        public static void setup() throws NoSuchFieldException {
+            fieldEmployees = EmployeeMaxSizeTenService.class.getDeclaredField("employees");
+            fieldEmployees.setAccessible(true);
+        }
+
+        @BeforeEach
+        public void initEach() throws IllegalAccessException {
             var testEmployees = new ArrayList<>(
                     List.of(
                             new Employee("Малышева", "Амалия"),
@@ -126,83 +55,211 @@ class EmployeeControllerTest {
                             new Employee("Романов", "Артём"),
                             new Employee("Широков", "Павел"),
                             new Employee("Кудрявцев", "Лев"),
-                            new Employee("Белякова", "Антонина"),
                             new Employee("Филиппова", "Алиса")
                     )
             );
             fieldEmployees.set(service, testEmployees);
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
-
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         }
-        @Test
-        void addAlreadyAdded() {
-            Employee testEmployee = new Employee("Кудрявцев", "Лев");
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
 
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
+        @Nested
+        class AllSuccess {
+            @Test
+            void add() throws IllegalAccessException {
+                Employee testEmployee = new Employee("Белякова", "Антонина");
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
 
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
+                var employees = (List<Employee>) fieldEmployees.get(service);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
+                assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
+                assertThat(employees.size()).isEqualTo(10);
+                assertThat(employees).contains(testEmployee);
+            }
+
+            @Test
+            void delete() throws IllegalAccessException {
+                Employee testEmployee = new Employee("Кудрявцев", "Лев");
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/delete?name={name}&surname={surname}", Employee.class, uriVariables);
+                var employees = (List<Employee>) fieldEmployees.get(service);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
+                assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
+                assertThat(employees.size()).isEqualTo(8);
+                assertThat(employees).doesNotContain(testEmployee);
+            }
+
+            @Test
+            void find() throws IllegalAccessException {
+                Employee testEmployee = new Employee("Кудрявцев", "Лев");
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/find?name={name}&surname={surname}", Employee.class, uriVariables);
+                var employees = (List<Employee>) fieldEmployees.get(service);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                assertThat(response.getBody().getSurname()).isEqualTo(uriVariables.get("surname"));
+                assertThat(response.getBody().getName()).isEqualTo(uriVariables.get("name"));
+                assertThat(employees).contains(testEmployee);
+                assertThat(employees.size()).isEqualTo(9);
+            }
         }
-        @Test
-        void deleteNotFound() {
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", "Антонина");
-            uriVariables.put("surname", "Белякова");
 
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/delete?name={name}&surname={surname}", Employee.class, uriVariables);
+        @Nested
+        class AllError {
+            @Test
+            void addStorageIsFull() throws IllegalAccessException {
+                Employee testEmployee = new Employee("Иванов", "Иван");
+                var testEmployees = new ArrayList<>(
+                        List.of(
+                                new Employee("Малышева", "Амалия"),
+                                new Employee("Козловский", "Денис"),
+                                new Employee("Соловьева", "Серафима"),
+                                new Employee("Макарова", "Дарья"),
+                                new Employee("Лебедева", "Таисия"),
+                                new Employee("Романов", "Артём"),
+                                new Employee("Широков", "Павел"),
+                                new Employee("Кудрявцев", "Лев"),
+                                new Employee("Белякова", "Антонина"),
+                                new Employee("Филиппова", "Алиса")
+                        )
+                );
+                fieldEmployees.set(service, testEmployees);
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
 
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void addAlreadyAdded() {
+                Employee testEmployee = new Employee("Кудрявцев", "Лев");
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void deleteNotFound() {
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", "Антонина");
+                uriVariables.put("surname", "Белякова");
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/delete?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+            }
+
+            @Test
+            void findNotFound() {
+                Employee testEmployee = new Employee("Иванов", "Иван");
+                Map<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("name", testEmployee.getName());
+                uriVariables.put("surname", testEmployee.getSurname());
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/find?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+            }
         }
-        @Test
-        void findNotFound() {
-            Employee testEmployee = new Employee("Иванов", "Иван");
-            Map<String, String> uriVariables = new HashMap<>();
-            uriVariables.put("name", testEmployee.getName());
-            uriVariables.put("surname", testEmployee.getSurname());
 
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/find?name={name}&surname={surname}", Employee.class, uriVariables);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        }
     }
+
 
     @Nested
-    class NoParameters{
-        @Test
-        void addNotFound() {
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/add", Employee.class);
+    class ParametersError {
+        @Nested
+        class NoParameters {
+            @Test
+            void addNotFound() {
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/add", Employee.class);
 
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void findNotFound() {
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/find", Employee.class);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void deleteNotFound() {
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/delete", Employee.class);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
         }
 
-        @Test
-        void findNotFound() {
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/find", Employee.class);
+        @Nested
+        class ParametersIsEmpty {
+            private static Map<String, String> uriVariables;
 
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-        }
+            @BeforeAll
+            public static void setup() {
+                uriVariables = new HashMap<>();
+                uriVariables.put("name", null);
+                uriVariables.put("surname", null);
+            }
 
-        @Test
-        void deleteNotFound() {
-            ResponseEntity<Employee> response
-                    = restTemplate.getForEntity("/employee/delete", Employee.class);
+            @Test
+            void add() {
 
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/add?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void delete() {
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/delete?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
+
+            @Test
+            void find() {
+
+                ResponseEntity<Employee> response
+                        = restTemplate.getForEntity("/employee/find?name={name}&surname={surname}", Employee.class, uriVariables);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            }
         }
 
     }
+
 
 }
